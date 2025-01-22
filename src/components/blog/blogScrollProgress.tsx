191bf056fe73852da0dd27005eb8af4f0bcc9aa5
katongole isaac <katongolelsaac78@gmail.com>
2025-01-22 17:31:33 +0300

    feat: Blog scroll progess bar added

---
+++ src/components/blog/blogScrollProgress.tsx
@@ -0,0 +1,23 @@
+import { motion, MotionValue, useSpring } from "framer-motion";
+
+interface Props {
+  scrollYProgress: MotionValue;
+}
+const BlogScrollProgress: React.FC<Props> = ({ scrollYProgress }) => {
+  const scaleX = useSpring(scrollYProgress);
+  return (
+    <motion.div
+      transition={{ type: "spring", duration: 0.8, bounce: 0.25 }}
+      style={{
+        scaleX,
+        position: "fixed",
+        originX: 0,
+        zIndex: 90, // remember mobile navbar zIndex is 100, you shudn't go over it
+      }}
+      className={`
+         bg-gradient-to-r from-rose-500/70 via-fuchsia-600/80 to-blue-500/50 fixed h-1 md:h-1 mt-[2.9rem] md:mt-[3.4rem] top-0 left-0 right-0`}
+    ></motion.div>
+  );
+};
+
+export default BlogScrollProgress;
